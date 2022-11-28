2ccf0598c7e61fba1a54e2f31414b5b6163a1e97
Hongda <67817916+Hongda-OSU@users.noreply.github.com>
2022-11-28 15:32:10 -0500

    layout

---
+++ heat_map.js
@@ -0,0 +1,10 @@
+function drawHeatMap(heatmap_data, margin, width, height) {
+    var svg = d3
+      .select("#heat_map")
+      .append("svg")
+      .attr("width", width + margin.left * 2 + margin.right * 2)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left / 5 + "," + margin.top + ")");
+
+}
---
+++ scatter_plot.js
@@ -0,0 +1,10 @@
+function drawScatterPlot(scatterplot_data, margin, width, height) {
+    var svg = d3
+      .select("#scatter_plot")
+      .append("svg")
+      .attr("width", width + margin.left * 2 + margin.right * 2)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left / 5 + "," + margin.top + ")");
+
+}
